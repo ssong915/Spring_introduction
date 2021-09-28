d3b66bab3e897d498b7511dbceccd6aeea4f42f6
ssong <yysk_915@naver.com>
2021-09-29 02:56:51 +0900

    [Add] API

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,5 +20,31 @@ public class HelloController {
         // RequestParam : 외부에서 string 받기
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //HTTP body에 내용을 직접 넣어주겠다!
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+        //그럼 위에 넘이랑 차이는? 태그들 하나도없이 return값만 전달되는 것
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
