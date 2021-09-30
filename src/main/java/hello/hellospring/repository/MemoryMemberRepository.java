3223d970eb44fae63d2ab7ddd2650757b0c0547f
ssong <yysk_915@naver.com>
2021-10-01 01:43:45 +0900

    [Add] 회원 리포지토리 테스트케이크 작성

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -4,15 +4,15 @@ import hello.hellospring.domain.Member;
 
 import java.util.*;
 
-public class MemoryMemberRepository<store> implements MemberRepository{
+public class MemoryMemberRepository implements MemberRepository{
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L; //0123... 키값 생성해주는 넘
 
     @Override
     public Member save(Member member) {
         member.setId(++sequence); //member id 값 생성
-        store.put(member.getId(), member); 
-        return member
+        store.put(member.getId(), member);
+        return member;
     }
 
     @Override
@@ -23,12 +23,16 @@ public class MemoryMemberRepository<store> implements MemberRepository{
     @Override
     public Optional<Member> findByName(String name) {
         return store.values().stream()
-                .filter(member->member.getName().equals(name))
+                .filter(member -> member.getName().equals(name))
                 .findAny();
     }
 
     @Override
     public List<Member> findAll() { //얘는 왜 리스트?
-        return new ArrayList<>(store.values()) //store에 있는 member들이 반환
+        return new ArrayList<>(store.values()); //store에 있는 member들이 반환
+    }
+
+    public void clearStore(){
+        store.clear();
     }
 }
